327a9da4a3457a857aaad5b290099a34d8a8e0a5
derek <wongwangfung@gmail.com>
2020-10-21 23:04:56 +0800

    First repair

--- android/src/main/java/sq/flutter/tflite/TflitePlugin.java
+++ android/src/main/java/sq/flutter/tflite/TflitePlugin.java
@@ -486,7 +486,7 @@ public class TflitePlugin implements MethodCallHandler {
     ByteBuffer x2;
     long startTime;
     Map<Integer, Object> outputs = new HashMap<>();
-    float[] similarity;
+    float[][] outputs;
     Object[] inputs;
 
     RunSiameseOnImages(HashMap args, Result result) throws IOException {
@@ -506,7 +506,7 @@ public class TflitePlugin implements MethodCallHandler {
       startTime = SystemClock.uptimeMillis();
       this.inputs = new Object[]{ x1 , x2 };
       
-      outputs.put(0, new float[1]);
+      outputs.put(0, new float[1][1]);
     }
 
     protected void runTflite() {
@@ -516,9 +516,12 @@ public class TflitePlugin implements MethodCallHandler {
     protected void onRunTfliteDone() {
       Log.v("time", "Inference took " + (SystemClock.uptimeMillis() - startTime));
       similarity = ( float[] )outputs.get( 0 ) ;
-
-      Log.v("similarity", "Similarity " + similarity[0]);
-      result.success(similarity[0]);
+      dissimilarity = ( float[] )outputs.get( 1 ) ;
+      Log.v("similarity", "Similarity " + similarity);
+      
+      Log.v("similarity", "Similarity " + similarity[0][0]);
+      Log.v("dissimilarity", "Disimilarity " + dissimilarity[0][0]);
+      result.success(similarity[0][0]);
     }
   }
 
